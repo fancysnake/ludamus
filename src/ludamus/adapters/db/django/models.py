55be656260b5fd05fe3bb9e0069d878c0d36bc14
Radosław Ganczarek <radoslaw@ganczarek.in>
2025-06-15 09:26:45 +0200

    Add messages

--- src/ludamus/adapters/db/django/models.py
+++ src/ludamus/adapters/db/django/models.py
@@ -80,7 +80,7 @@ class User(AbstractBaseUser, PermissionsMixin):
         null=True,
         related_name="connected",
     )
-    name = models.CharField(_("Name"), max_length=255, blank=True)
+    name = models.CharField(_("User name"), max_length=255, blank=True)
     slug = models.SlugField(unique=True, db_index=True)
     user_type = models.CharField(
         max_length=255, choices=UserType, default=UserType.ACTIVE
