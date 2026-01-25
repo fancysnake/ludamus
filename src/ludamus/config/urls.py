b69ad032ca960e26e6573eec352e4b124dd64c4e
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2026-01-25 22:43:33 +0100

    Add debug URLs for testing 404/500 error pages

--- src/ludamus/config/urls.py
+++ src/ludamus/config/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 
 """
 
+from typing import NoReturn
+
 from django.conf import settings
 from django.contrib import admin
+from django.http import HttpRequest  # noqa: TC002  # used in debug view
 from django.urls import include, path
 
 urlpatterns = [
@@ -38,15 +41,17 @@ if settings.DEBUG:
     urlpatterns += [path("__reload__/", include("django_browser_reload.urls"))]
 
     # Debug URLs to test error pages
-    def _trigger_500(request):  # noqa: ARG001
-        raise Exception("Test 500 error")  # noqa: TRY002
+    def _trigger_500(request: HttpRequest) -> NoReturn:  # noqa: ARG001
+        raise Exception(  # noqa: TRY002  # pylint: disable=broad-exception-raised
+            "Test 500 error"
+        )
 
     from ludamus.adapters.web.django.error_views import custom_404, custom_500
 
     urlpatterns += [
-        path("404/", lambda r: custom_404(r, None)),
-        path("500/", custom_500),
-        path("500-real/", _trigger_500),  # triggers actual 500 (shows debug page)
+        path("404/", lambda r: custom_404(r, None)),  # type: ignore[list-item]
+        path("500/", custom_500),  # type: ignore[list-item]
+        path("500-real/", _trigger_500),  # type: ignore[list-item]
     ]
 
 if settings.DEBUG and "debug_toolbar" in settings.INSTALLED_APPS:
