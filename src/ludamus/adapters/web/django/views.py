384edb6707dfef41bc03c94b4da91112edb91e83
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2026-01-28 00:16:52 +0100

    Fix post-merge errors

--- src/ludamus/adapters/web/django/views.py
+++ src/ludamus/adapters/web/django/views.py
@@ -97,6 +97,10 @@ class LoginRequiredPageView(TemplateView):
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
         context["next"] = self.request.GET.get("next", "")
+        # Variables for login_button.html component
+        context["show_icon"] = True
+        context["text"] = ""
+        context["extra_class"] = ""
         return context
 
 
